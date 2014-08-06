74001b6e2e86ecb1595c760a3286cf88356fac99
xindizhiyin <Jack@jackdembp.lan>
2014-08-06 17:29:10 +0800

    Initial Commit

---
+++ ZhuZhan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZhuZhan
+//
+//  Created by Jack on 14-8-6.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZhuZhan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZhuZhan
+//
+//  Created by Jack on 14-8-6.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
