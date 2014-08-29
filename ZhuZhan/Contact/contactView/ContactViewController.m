7388850920b66fa42a16bdd88f9735b5b407e0f0
孙元侃 <sunyuankan@sunyuankandeMacBook-Pro.local>
2014-08-29 10:07:50 +0800

    孙元侃

--- ZhuZhan/Contact/contactView/ContactViewController.m
+++ ZhuZhan/Contact/contactView/ContactViewController.m
@@ -100,26 +100,26 @@ static int rowNum =0;
 {
     _datasource = [NSMutableArray new];
     
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *components = [[NSDateComponents alloc] init];
-    NSDate *today = [NSDate date];
-    NSDateComponents *todayComponents = [calendar components:NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit fromDate:today];
+//    NSCalendar *calendar = [NSCalendar currentCalendar];
+//    NSDateComponents *components = [[NSDateComponents alloc] init];
+//    NSDate *today = [NSDate date];
+//    NSDateComponents *todayComponents = [calendar components:NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit fromDate:today];
+//    
+//    for (NSInteger i = [todayComponents day]; i >= -15; i--)
+//    {
+//        [components setYear:[todayComponents year]];
+//        [components setMonth:[todayComponents month]];
+//        [components setDay:i];
+//        [components setHour:arc4random() % 23];
+//        [components setMinute:arc4random() % 59];
+//        
+//        NSDate *date = [calendar dateFromComponents:components];
+//        [_datasource addObject:date];
+//    }
     
-    for (NSInteger i = [todayComponents day]; i >= -15; i--)
-    {
-        [components setYear:[todayComponents year]];
-        [components setMonth:[todayComponents month]];
-        [components setDay:i];
-        [components setHour:arc4random() % 23];
-        [components setMinute:arc4random() % 59];
-        
-        NSDate *date = [calendar dateFromComponents:components];
-        [_datasource addObject:date];
-    }
-    
-    /*for(int i=0;i<30;i++){
+    for(int i=0;i<30;i++){
         [_datasource addObject:[NSDate date]];
-    }*/
+    }
 }
 
 - (void)_refreshing {
@@ -155,68 +155,68 @@ static int rowNum =0;
 }
 /******************************************************************************************************************/
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:PSTableViewCellIdentifier];
-    
-    NSDate *date = _datasource[[indexPath row]];
-    NSTimeZone *zone = [NSTimeZone systemTimeZone];
-    
-    NSInteger interval = [zone secondsFromGMTForDate: date];
-    
-    if (indexPath.row==0 ||indexPath.row==1) {
-    
-        
-        static  NSString *identifier2 = @"Cell1";
-        Cell1 * cell2 = (Cell1 *)[tableView dequeueReusableCellWithIdentifier:identifier2];
-        if (!cell2) {
-            cell2 = [[Cell1 alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier2];
-        }
-        
-        cell2.cellIcon.image = [UIImage imageNamed:@"面部采集_12"];
-        [cell2.userIcon setBackgroundImage:[UIImage imageNamed:@"1"] forState:UIControlStateNormal];
-        [cell2.userIcon addTarget:self action:@selector(ShowUserPanView:) forControlEvents:UIControlEventTouchUpInside];
-        cell2.userIcon.tag = indexPath.row;
-        cell2.contentLabel.text = @"显示文字";
-        cell2.selectionStyle = UITableViewCellSelectionStyleNone;
-        return cell2;
-        
-        
-        
-    }
-    if (indexPath.row==2){
-        static  NSString *identifier3 = @"CompanyPublishedCell";
-        CompanyPublishedCell * cell3 = (CompanyPublishedCell *)[tableView dequeueReusableCellWithIdentifier:identifier3];
-        if (!cell3) {
-            cell3 = [[CompanyPublishedCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier3];
-        }
-        cell3.bigImgView.image = [UIImage imageNamed:@"123"];
-        NSString *tempStr = @"           ";
-        cell3.publishView.text =[tempStr stringByAppendingString:@"实现水平方向滑动的UIPicker。自定义UIPicker。如果你厌倦了iOS自带的UIPicker，那么你可以试试这个不一样的UIPicker。不仅仅可以用作选择器，也可以用作菜单"];
-        cell3.userNameLabel.text = @"Jack:";
-        cell3.userNameLabel.textColor = [UIColor blueColor];
-        cell3.selectionStyle = UITableViewCellSelectionStyleNone;
-        return cell3;
-    }
-    
-    static  NSString *identifier4 = @"CommentsCell";
-    CommentsCell * cell4 = (CommentsCell *)[tableView dequeueReusableCellWithIdentifier:identifier4];
-    if (!cell4) {
-        
-        cell4 = [[CommentsCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier4];
-    }
-    [cell4.userIcon setBackgroundImage:[UIImage imageNamed:@"1"] forState:UIControlStateNormal];
-    [cell4.userIcon addTarget:self action:@selector(ShowUserPanView:) forControlEvents:UIControlEventTouchUpInside];
-    cell4.userIcon.tag = indexPath.row;
-    NSString *tempStr = @"           ";
-    cell4.commentsView.text =[tempStr stringByAppendingString:@"实现水平方向滑动的UIPicker。"];
-    cell4.userNameLabel.text = @"Lisa:";
-    cell4.commentsView.textColor = [UIColor blueColor];
-    cell4.selectionStyle = UITableViewCellSelectionStyleNone;
-    return cell4;
-    
-    return cell;
-}
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:PSTableViewCellIdentifier];
+//    
+//    NSDate *date = _datasource[[indexPath row]];
+//    NSTimeZone *zone = [NSTimeZone systemTimeZone];
+//    
+//    NSInteger interval = [zone secondsFromGMTForDate: date];
+//    
+//    if (indexPath.row==0 ||indexPath.row==1) {
+//    
+//        
+//        static  NSString *identifier2 = @"Cell1";
+//        Cell1 * cell2 = (Cell1 *)[tableView dequeueReusableCellWithIdentifier:identifier2];
+//        if (!cell2) {
+//            cell2 = [[Cell1 alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier2];
+//        }
+//        
+//        cell2.cellIcon.image = [UIImage imageNamed:@"面部采集_12"];
+//        [cell2.userIcon setBackgroundImage:[UIImage imageNamed:@"1"] forState:UIControlStateNormal];
+//        [cell2.userIcon addTarget:self action:@selector(ShowUserPanView:) forControlEvents:UIControlEventTouchUpInside];
+//        cell2.userIcon.tag = indexPath.row;
+//        cell2.contentLabel.text = @"显示文字";
+//        cell2.selectionStyle = UITableViewCellSelectionStyleNone;
+//        return cell2;
+//        
+//        
+//        
+//    }
+//    if (indexPath.row==2){
+//        static  NSString *identifier3 = @"CompanyPublishedCell";
+//        CompanyPublishedCell * cell3 = (CompanyPublishedCell *)[tableView dequeueReusableCellWithIdentifier:identifier3];
+//        if (!cell3) {
+//            cell3 = [[CompanyPublishedCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier3];
+//        }
+//        cell3.bigImgView.image = [UIImage imageNamed:@"123"];
+//        NSString *tempStr = @"           ";
+//        cell3.publishView.text =[tempStr stringByAppendingString:@"实现水平方向滑动的UIPicker。自定义UIPicker。如果你厌倦了iOS自带的UIPicker，那么你可以试试这个不一样的UIPicker。不仅仅可以用作选择器，也可以用作菜单"];
+//        cell3.userNameLabel.text = @"Jack:";
+//        cell3.userNameLabel.textColor = [UIColor blueColor];
+//        cell3.selectionStyle = UITableViewCellSelectionStyleNone;
+//        return cell3;
+//    }
+//    
+//    static  NSString *identifier4 = @"CommentsCell";
+//    CommentsCell * cell4 = (CommentsCell *)[tableView dequeueReusableCellWithIdentifier:identifier4];
+//    if (!cell4) {
+//        
+//        cell4 = [[CommentsCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier4];
+//    }
+//    [cell4.userIcon setBackgroundImage:[UIImage imageNamed:@"1"] forState:UIControlStateNormal];
+//    [cell4.userIcon addTarget:self action:@selector(ShowUserPanView:) forControlEvents:UIControlEventTouchUpInside];
+//    cell4.userIcon.tag = indexPath.row;
+//    NSString *tempStr = @"           ";
+//    cell4.commentsView.text =[tempStr stringByAppendingString:@"实现水平方向滑动的UIPicker。"];
+//    cell4.userNameLabel.text = @"Lisa:";
+//    cell4.commentsView.textColor = [UIColor blueColor];
+//    cell4.selectionStyle = UITableViewCellSelectionStyleNone;
+//    return cell4;
+//    
+//    return cell;
+//}
 
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
