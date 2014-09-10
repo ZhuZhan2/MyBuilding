b3356c5d6b43dadc7aa0e2b01df40d914006071b
xindizhiyin <Jack@JackdeMacBook-Pro.local>
2014-09-10 15:01:23 +0800

    人的详情  tababr 隐藏

--- ZhuZhan/Contact/personalDetail/PersonalDetailViewController.m
+++ ZhuZhan/Contact/personalDetail/PersonalDetailViewController.m
@@ -8,7 +8,8 @@
 
 #import "PersonalDetailViewController.h"
 #import "ProgramDetailViewController.h"
-
+#import "AppDelegate.h"
+#import "HomePageViewController.h"
 @interface PersonalDetailViewController ()
 
 @end
@@ -123,6 +124,23 @@ static int projectNum =0;
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         [wself.pathCover stopRefresh];
     });
+}
+/******************************************************************************************************************/
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    //恢复tabBar
+    AppDelegate* app=[AppDelegate instance];
+    HomePageViewController* homeVC=(HomePageViewController*)app.window.rootViewController;
+    [homeVC homePageTabBarRestore];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    //隐藏tabBar
+    AppDelegate* app=[AppDelegate instance];
+    HomePageViewController* homeVC=(HomePageViewController*)app.window.rootViewController;
+    [homeVC homePageTabBarHide];
 }
 
 /******************************************************************************************************************/
@@ -316,6 +334,7 @@ static int projectNum =0;
 //**************************************************************************************************************
 -(void)onClickAttention:(int)tag
 {
+    
     if ([MFMailComposeViewController canSendMail]){
         // Email Subject
         NSString *emailTitle = nil;
@@ -332,7 +351,9 @@ static int projectNum =0;
         [mc setToRecipients:toRecipents];
         
         // Present mail view controller on screen
-        [self presentViewController:mc animated:YES completion:NULL];
+       [self.view.window.rootViewController presentViewController:mc animated:YES completion:NULL];
+
+
     }else{
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示"
                                                         message:@"此设备不支持发送邮件"
@@ -349,6 +370,7 @@ static int projectNum =0;
 
 - (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
 {
+    NSLog(@"ghfhgfhgf");
     switch (result)
     {
         case MFMailComposeResultCancelled:
@@ -369,7 +391,10 @@ static int projectNum =0;
     
     
     // Close the Mail Interface
-    [self dismissViewControllerAnimated:YES completion:NULL];
+    [controller dismissViewControllerAnimated:YES completion:nil];
+
+
+
 }
 
 
