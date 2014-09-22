cc915d978462cef3a61dafe70de462d96128863b
xindizhiyin <Jack@JackdeMacBook-Pro.local>
2014-09-22 17:13:17 +0800

    11

--- ZhuZhan/AppDelegate.m
+++ ZhuZhan/AppDelegate.m
@@ -61,53 +61,53 @@
     [RecordSqlite opensql];
     
     
-//    if(![[NSUserDefaults standardUserDefaults] boolForKey:@"firstLaunch"]){
-//        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"firstLaunch"];
-//        NSLog(@"第一次启动");
-//        LoginViewController *loginview = [[LoginViewController alloc] init];
-//        UINavigationController *naVC = [[UINavigationController alloc] initWithRootViewController:loginview];
-//        
-//        [self.window setRootViewController:naVC];
-//        self.window.backgroundColor = [UIColor whiteColor];
-//        [self.window makeKeyAndVisible];
-//    }else{
-//        NSLog(@"==>%@",[[NSUserDefaults standardUserDefaults]objectForKey:@"deviceToken"]);
-//        if (![[NSUserDefaults standardUserDefaults]objectForKey:@"deviceToken"]) {
-//            LoginViewController *loginview = [[LoginViewController alloc] init];
-//            UINavigationController *naVC = [[UINavigationController alloc] initWithRootViewController:loginview];
-//            [self.window setRootViewController:naVC];
-//            self.window.backgroundColor = [UIColor whiteColor];
-//            [self.window makeKeyAndVisible];
-//        }else{
+    if(![[NSUserDefaults standardUserDefaults] boolForKey:@"firstLaunch"]){
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"firstLaunch"];
+        NSLog(@"第一次启动");
+        LoginViewController *loginview = [[LoginViewController alloc] init];
+        UINavigationController *naVC = [[UINavigationController alloc] initWithRootViewController:loginview];
+        
+        [self.window setRootViewController:naVC];
+        self.window.backgroundColor = [UIColor whiteColor];
+        [self.window makeKeyAndVisible];
+    }else{
+        NSLog(@"==>%@",[[NSUserDefaults standardUserDefaults]objectForKey:@"deviceToken"]);
+        if (![[NSUserDefaults standardUserDefaults]objectForKey:@"deviceToken"]) {
+            LoginViewController *loginview = [[LoginViewController alloc] init];
+            UINavigationController *naVC = [[UINavigationController alloc] initWithRootViewController:loginview];
+            [self.window setRootViewController:naVC];
+            self.window.backgroundColor = [UIColor whiteColor];
+            [self.window makeKeyAndVisible];
+        }else{
     
-//            #if TARGET_IPHONE_SIMULATOR
+            #if TARGET_IPHONE_SIMULATOR
             HomePageViewController *homeVC = [[HomePageViewController alloc] init];
             self.window.rootViewController = homeVC;
             [self.window makeKeyAndVisible];
-//            #elif TARGET_OS_IPHONE
-//            if([[networkConnect sharedInstance] connectedToNetwork]){
-//                NSLog(@"%@",[[NSUserDefaults standardUserDefaults]objectForKey:@"isFaceRegister"]);
-//                if ([[[NSUserDefaults standardUserDefaults]objectForKey:@"isFaceRegister"] isEqualToString:@"0"]) {
-//                    LoginViewController *loginview = [[LoginViewController alloc] init];
-//                    UINavigationController *naVC = [[UINavigationController alloc] initWithRootViewController:loginview];
-//                    [self.window setRootViewController:naVC];
-//                    self.window.backgroundColor = [UIColor whiteColor];
-//                    [self.window makeKeyAndVisible];
-//                }else{
-//                    FaceLoginViewController *faceVC = [[FaceLoginViewController alloc] init];
-//                    UINavigationController *naVC = [[UINavigationController alloc] initWithRootViewController:faceVC];
-//                    [self.window setRootViewController:naVC];
-//                    self.window.backgroundColor = [UIColor whiteColor];
-//                    [self.window makeKeyAndVisible];
-//                }
-//            }else{
-//                HomePageViewController *homeVC = [[HomePageViewController alloc] init];
-//                self.window.rootViewController = homeVC;
-//                [self.window makeKeyAndVisible];
-//            }
-//            #endif
-//        }
-//    }
+            #elif TARGET_OS_IPHONE
+            if([[networkConnect sharedInstance] connectedToNetwork]){
+                NSLog(@"%@",[[NSUserDefaults standardUserDefaults]objectForKey:@"isFaceRegister"]);
+                if ([[[NSUserDefaults standardUserDefaults]objectForKey:@"isFaceRegister"] isEqualToString:@"0"]) {
+                    LoginViewController *loginview = [[LoginViewController alloc] init];
+                    UINavigationController *naVC = [[UINavigationController alloc] initWithRootViewController:loginview];
+                    [self.window setRootViewController:naVC];
+                    self.window.backgroundColor = [UIColor whiteColor];
+                    [self.window makeKeyAndVisible];
+                }else{
+                    FaceLoginViewController *faceVC = [[FaceLoginViewController alloc] init];
+                    UINavigationController *naVC = [[UINavigationController alloc] initWithRootViewController:faceVC];
+                    [self.window setRootViewController:naVC];
+                    self.window.backgroundColor = [UIColor whiteColor];
+                    [self.window makeKeyAndVisible];
+                }
+            }else{
+                HomePageViewController *homeVC = [[HomePageViewController alloc] init];
+                self.window.rootViewController = homeVC;
+                [self.window makeKeyAndVisible];
+            }
+            #endif
+        }
+    }
     return YES;
 }
 
