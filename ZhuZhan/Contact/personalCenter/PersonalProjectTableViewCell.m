e7de2e047686a8a82a5c67cbf7d6b80305427541
wangy322@sina.com <wangyang@wangyangdeMacBook-Air-2.local>
2014-09-28 10:39:11 +0800

    wy

--- ZhuZhan/Contact/personalCenter/PersonalProjectTableViewCell.m
+++ ZhuZhan/Contact/personalCenter/PersonalProjectTableViewCell.m
@@ -33,7 +33,24 @@
 }
 
 -(void)addContent{
+    UIImageView *lineImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 49, 320, 1)];
+    [lineImage setBackgroundColor:[UIColor blackColor]];
+    [self.contentView addSubview:lineImage];
+    lineImage.alpha = 0.1;
     
+    stageImage = [[UIImageView alloc] initWithFrame:CGRectMake(53.5, 11.5, 27, 27)];
+    [self.contentView addSubview:stageImage];
+    
+    titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 5, 200, 20)];
+    titleLabel.font = [UIFont fontWithName:@"GurmukhiMN-Bold" size:14];
+    titleLabel.textAlignment = NSTextAlignmentLeft;
+    [self.contentView addSubview:titleLabel];
+    
+    contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 25, 200, 20)];
+    contentLabel.font = [UIFont fontWithName:@"GurmukhiMN" size:12];
+    contentLabel.textAlignment = NSTextAlignmentLeft;
+    contentLabel.textColor = [UIColor lightGrayColor];
+    [self.contentView addSubview:contentLabel];
 }
 
 -(void)setModel:(PersonalCenterModel *)model{
