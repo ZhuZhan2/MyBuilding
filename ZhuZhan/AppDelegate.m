ee1b27f775a59039b6ddd5d656e5e0e34c5de0ed
wobushigogo <wangy322@sina.com>
2014-10-23 10:11:45 +0800

    wy

--- ZhuZhan/AppDelegate.m
+++ ZhuZhan/AppDelegate.m
@@ -26,7 +26,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
+    NSLog(@"asdfsadfsadf");
     //设置log等级，此处log为默认在documents目录下的msc.log文件
     [IFlySetting setLogFile:LVL_NONE];
     
