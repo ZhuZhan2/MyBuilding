c1eb02770de94eee46e8abccd3994cf8177b4b55
wobushigogo <wangy322@sina.com>
2014-11-12 19:36:05 +0800

    wy

--- ZhuZhan/Model/ContactModel/ContactModel.m
+++ ZhuZhan/Model/ContactModel/ContactModel.m
@@ -255,8 +255,10 @@
     NSString *urlStr = [NSString stringWithFormat:@"api/CompanyBaseInformation/AddCompanyImages"];
     return [[AFAppDotNetAPIClient sharedClient]POST:urlStr parameters:dic success:^(NSURLSessionDataTask *task, id responseObject) {
         NSLog(@"responseObject ==> %@",responseObject);
+        NSMutableArray *mutablePosts = [[NSMutableArray alloc] init];
+        [mutablePosts addObject:responseObject[@"d"][@"data"]];
         if (block) {
-            block([NSMutableArray array],nil);
+            block(mutablePosts,nil);
         }
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
         if (block) {
