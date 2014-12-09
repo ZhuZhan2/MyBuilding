161e667445fb2a0aa27c6bf3d6e0d9f18a1750a8
rocksunzpz <rocksunzpz@163.com>
2014-12-09 14:01:51 +0800

    孙元侃

--- ZhuZhan/AppDelegate.m
+++ ZhuZhan/AppDelegate.m
@@ -73,8 +73,7 @@
     self.window.rootViewController = homeVC;
     [self.window makeKeyAndVisible];
 
-    if(1||![[NSUserDefaults standardUserDefaults] objectForKey:@"firstLaunch"]){
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"firstLaunch"];
+    if(![[NSUserDefaults standardUserDefaults] objectForKey:@"firstLaunch"]){
         NSLog(@"第一次启动");
         FirstOpenAppAnimationView* firstAnimationView=[[FirstOpenAppAnimationView alloc]initWithFrame:self.window.frame];
         [self.window addSubview:firstAnimationView];
