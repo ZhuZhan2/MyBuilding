bf114f442a2665f21a36552909a687a681049e5f
wobushigogo <wangy322@sina.com>
2014-12-18 18:25:56 +0800

    wy

--- ZhuZhan/LoginView/LoginViewController.h
+++ ZhuZhan/LoginView/LoginViewController.h
@@ -8,14 +8,16 @@
 
 #import <UIKit/UIKit.h>
 #import "RegistViewController.h"
+#import "WelcomeViewController.h"
 @protocol LoginViewDelegate <NSObject>
 -(void)loginCompleteWithDelayBlock:(void(^)())block;
 -(void)loginComplete;
 @end
 
-@interface LoginViewController : UIViewController<UITextFieldDelegate,UIAlertViewDelegate,registViewDelegate>{
+@interface LoginViewController : UIViewController<UITextFieldDelegate,UIAlertViewDelegate,registViewDelegate,WelcomeViewControllerdelegate>{
     UITextField *_userNameTextField;
     UITextField *_passWordTextField;
+    WelcomeViewController *wel;
 }
 @property(nonatomic,weak)id<LoginViewDelegate>delegate;
 @property(nonatomic)BOOL needDelayCancel;
