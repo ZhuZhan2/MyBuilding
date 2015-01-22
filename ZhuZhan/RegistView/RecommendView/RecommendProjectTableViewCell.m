fb1fe77b9edaffbec7ff67a3e00b67e5b48048a5
wobushigogo <wangy322@sina.com>
2015-01-22 15:56:29 +0800

    wy

--- ZhuZhan/RegistView/RecommendView/RecommendProjectTableViewCell.m
+++ ZhuZhan/RegistView/RecommendView/RecommendProjectTableViewCell.m
@@ -19,7 +19,7 @@
 }
 
 -(void)addContent{
-    stageImage = [[UIImageView alloc] initWithFrame:CGRectMake(15, 15, 53, 53)];
+    stageImage = [[UIImageView alloc] initWithFrame:CGRectMake(15, 14, 53, 53)];
     stageImage.image = [GetImagePath getImagePath:@"+项目-首页_21a"];
     [self.contentView addSubview:stageImage];
     
@@ -28,59 +28,56 @@
     lineImage.alpha = 0.2;
     [self.contentView addSubview:lineImage];
     
-    startDate = [[UILabel alloc] initWithFrame:CGRectMake(13, 65, 58, 20)];
+    startDate = [[UILabel alloc] initWithFrame:CGRectMake(13, 72, 60, 13)];
     startDate.text = @"2014/15/5";
-    startDate.font = [UIFont systemFontOfSize:10];
+    startDate.font = [UIFont systemFontOfSize:11];
     startDate.textColor = [UIColor blackColor];
     startDate.textAlignment = NSTextAlignmentCenter;
     [self.contentView addSubview:startDate];
     
-    endDate = [[UILabel alloc] initWithFrame:CGRectMake(13, 80, 58, 20)];
+    endDate = [[UILabel alloc] initWithFrame:CGRectMake(13, 83, 60, 13)];
     endDate.text = @"2014/15/21";
-    endDate.font = [UIFont systemFontOfSize:10];
+    endDate.font = [UIFont systemFontOfSize:11];
     endDate.textColor = [UIColor redColor];
     endDate.textAlignment = NSTextAlignmentCenter;
     [self.contentView addSubview:endDate];
     
-    projectName = [[UILabel alloc] initWithFrame:CGRectMake(83, 17, 150, 17)];
+    projectName = [[UILabel alloc] initWithFrame:CGRectMake(87, 17, 180, 17)];
     projectName.font = [UIFont systemFontOfSize:17];
     [self.contentView addSubview:projectName];
     
-    UILabel *areaLabel = [[UILabel alloc] initWithFrame:CGRectMake(83, 42, 60, 14)];
+    UILabel *areaLabel = [[UILabel alloc] initWithFrame:CGRectMake(87, 42, 60, 13)];
     areaLabel.textColor = RGBCOLOR(40, 134, 247);
     areaLabel.text = @"建筑面积";
-    areaLabel.font = [UIFont systemFontOfSize:14];
+    areaLabel.font = [UIFont systemFontOfSize:13];
     [self.contentView addSubview:areaLabel];
     
-    area = [[UILabel alloc] initWithFrame:CGRectMake(145, 42, 120, 14)];
-    area.textColor = RGBCOLOR(106, 106, 106);
-    area.font = [UIFont systemFontOfSize:14];
+    area = [[UILabel alloc] initWithFrame:CGRectMake(151, 42, 120, 13)];
+    area.font = [UIFont systemFontOfSize:13];
     [self.contentView addSubview:area];
     
-    UILabel *investmentLabel = [[UILabel alloc] initWithFrame:CGRectMake(83, 62, 50, 14)];
+    UILabel *investmentLabel = [[UILabel alloc] initWithFrame:CGRectMake(87, 61, 50, 13)];
     investmentLabel.textColor = RGBCOLOR(40, 134, 247);
     investmentLabel.text = @"投资额";
-    investmentLabel.font = [UIFont systemFontOfSize:14];
+    investmentLabel.font = [UIFont systemFontOfSize:13];
     [self.contentView addSubview:investmentLabel];
     
-    investment = [[UILabel alloc] initWithFrame:CGRectMake(130, 62, 140, 14)];
-    investment.textColor = RGBCOLOR(106, 106, 106);
-    investment.font = [UIFont systemFontOfSize:14];
+    investment = [[UILabel alloc] initWithFrame:CGRectMake(140, 61, 140, 13)];
+    investment.font = [UIFont systemFontOfSize:13];
     [self.contentView addSubview:investment];
     
-    zone = [[UILabel alloc] initWithFrame:CGRectMake(83, 82, 70, 14)];
-    zone.textColor = RGBCOLOR(40, 134, 247);
-    zone.font = [UIFont systemFontOfSize:14];
+    zone = [[UILabel alloc] initWithFrame:CGRectMake(87, 79, 60, 13)];
+    zone.font = [UIFont systemFontOfSize:13];
     [self.contentView addSubview:zone];
     
-    address = [[UILabel alloc] initWithFrame:CGRectMake(140, 82, 140, 14)];
+    address = [[UILabel alloc] initWithFrame:CGRectMake(140, 79, 140, 13)];
     address.textColor = RGBCOLOR(106, 106, 106);
-    address.font = [UIFont systemFontOfSize:14];
+    address.font = [UIFont systemFontOfSize:13];
     [self.contentView addSubview:address];
     
     attentionBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     [attentionBtn setBackgroundImage:[GetImagePath getImagePath:@"公司认证员工_18a"] forState:UIControlStateNormal];
-    attentionBtn.frame = CGRectMake(272, 40.5, 26, 26);
+    attentionBtn.frame = CGRectMake(272, 41, 26, 26);
     [attentionBtn addTarget:self action:@selector(attentionBtnAction) forControlEvents:UIControlEventTouchUpInside];
     [self.contentView addSubview:attentionBtn];
 }
@@ -96,14 +93,51 @@
     }
     
     if([model.a_area isEqualToString:@""]){
-    
+        area.text = @"-";
+        area.textColor = GrayColor;
+    }else{
+        area.text = [NSString stringWithFormat:@"%@M²",model.a_area];
+        area.textColor = RGBCOLOR(106, 106, 106);
     }
-    area.text = [NSString stringWithFormat:@"%@M²",model.a_area];
-    investment.text = [NSString stringWithFormat:@"¥%@百万",model.a_investment];
-    zone.text = model.a_city;
-    address.text = model.a_landAddress;
-    startDate.text = [model.a_exceptStartTime stringByReplacingOccurrencesOfString:@"-" withString:@"/"];
-    endDate.text = [model.a_exceptFinishTime stringByReplacingOccurrencesOfString:@"-" withString:@"/"];
+    
+    if([model.a_investment isEqualToString:@""]){
+        investment.text = @"-";
+        investment.textColor = GrayColor;
+    }else{
+        investment.text = [NSString stringWithFormat:@"¥%@百万",model.a_investment];
+        investment.textColor = RGBCOLOR(106, 106, 106);
+    }
+    
+    if([model.a_city isEqualToString:@""]){
+        zone.text = @"区域 - ";
+        zone.textColor = GrayColor;
+    }else{
+        zone.text = [NSString stringWithFormat:@"%@ - ",model.a_city];
+        zone.textColor = RGBCOLOR(40, 134, 247);
+    }
+    
+    if([model.a_landAddress isEqualToString:@""]){
+        address.text = @"地址";
+        address.textColor = GrayColor;
+    }else{
+        address.text = model.a_landAddress;
+        address.textColor = RGBCOLOR(106, 106, 106);
+    }
+    
+    if([model.a_exceptStartTime isEqualToString:@""]){
+        startDate.text = @"开工日期";
+        startDate.textColor = GrayColor;
+    }else{
+        startDate.text = [model.a_exceptStartTime stringByReplacingOccurrencesOfString:@"-" withString:@"/"];
+    }
+    
+    if([model.a_exceptFinishTime isEqualToString:@""]){
+        endDate.text = @"竣工日期";
+        endDate.textColor = GrayColor;
+    }else{
+        endDate.text = [model.a_exceptFinishTime stringByReplacingOccurrencesOfString:@"-" withString:@"/"];
+    }
+    
     if([model.a_projectstage isEqualToString:@"1"]){
         [stageImage setImage:[GetImagePath getImagePath:@"+项目-首页_21a"]];
     }else if([model.a_projectstage isEqualToString:@"2"]){
