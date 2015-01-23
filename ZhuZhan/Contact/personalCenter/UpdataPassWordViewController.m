52f13dbb7bb9867763da0ed744a7ce8bc2ada346
wobushigogo <wangy322@sina.com>
2015-01-23 14:52:25 +0800

    wy

--- ZhuZhan/Contact/personalCenter/UpdataPassWordViewController.m
+++ ZhuZhan/Contact/personalCenter/UpdataPassWordViewController.m
@@ -14,6 +14,7 @@
 #import "MD5.h"
 #import "RemindView.h"
 #import "MBProgressHUD.h"
+#import "EndEditingGesture.h"
 @interface UpdataPassWordViewController ()
 
 @end
@@ -34,7 +35,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"修改密码";
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.backgroundColor = RGBCOLOR(245, 246, 248);
     //LeftButton设置属性
     UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [leftButton setFrame:CGRectMake(0, 0, 25, 22)];
@@ -43,71 +44,79 @@
     UIBarButtonItem *leftButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftButton];
     self.navigationItem.leftBarButtonItem = leftButtonItem;
     
-    UILabel *oldPassWord = [[UILabel alloc] initWithFrame:CGRectMake(15, 70, 60, 30)];
-    oldPassWord.textColor = BlueColor;
-    oldPassWord.text = @"原密码";
-    oldPassWord.font = [UIFont fontWithName:@"GurmukhiMN" size:14];
-    [self.view addSubview:oldPassWord];
+    UIView* topView=[[UIView alloc]initWithFrame:CGRectMake(0, 80, 320, 141)];
+    topView.backgroundColor=[UIColor whiteColor];
+    [self.view addSubview:topView];
+    UIImageView* backView=[[UIImageView alloc]initWithImage:[GetImagePath getImagePath:@"注册_04"]];
+    [topView addSubview:backView];
     
-    oldPassWordTextField = [[UITextField alloc] initWithFrame:CGRectMake(92, 70, 200, 30)];
+//    UILabel *oldPassWord = [[UILabel alloc] initWithFrame:CGRectMake(15, 70, 60, 30)];
+//    oldPassWord.textColor = BlueColor;
+//    oldPassWord.text = @"原密码";
+//    oldPassWord.font = [UIFont fontWithName:@"GurmukhiMN" size:14];
+//    [self.view addSubview:oldPassWord];
+    
+    oldPassWordTextField = [[UITextField alloc] initWithFrame:CGRectMake(22,4,276,47)];
     oldPassWordTextField.placeholder = @"原密码";
     oldPassWordTextField.font = [UIFont systemFontOfSize:14];
     oldPassWordTextField.delegate = self;
     oldPassWordTextField.secureTextEntry = YES;
     oldPassWordTextField.clearButtonMode = UITextFieldViewModeAlways;
-    [oldPassWordTextField setValue:[UIFont boldSystemFontOfSize:14] forKeyPath:@"_placeholderLabel.font"];
-    [self.view addSubview:oldPassWordTextField];
+    //[oldPassWordTextField setValue:[UIFont boldSystemFontOfSize:14] forKeyPath:@"_placeholderLabel.font"];
+    [topView addSubview:oldPassWordTextField];
     
-    UIImageView *lineImage = [[UIImageView alloc] initWithFrame:CGRectMake(15, 105, 290, 1)];
+    UIImageView *lineImage = [[UIImageView alloc] initWithFrame:CGRectMake(20, 99, 280, 1)];
     [lineImage setBackgroundColor:[UIColor blackColor]];
-    [self.view addSubview:lineImage];
+    [topView addSubview:lineImage];
     lineImage.alpha = 0.2;
     
-    UILabel *newPassWord = [[UILabel alloc] initWithFrame:CGRectMake(15, 115, 60, 30)];
-    newPassWord.textColor = BlueColor;
-    newPassWord.text = @"新密码";
-    newPassWord.font = [UIFont fontWithName:@"GurmukhiMN" size:14];
-    [self.view addSubview:newPassWord];
+//    UILabel *newPassWord = [[UILabel alloc] initWithFrame:CGRectMake(15, 115, 60, 30)];
+//    newPassWord.textColor = BlueColor;
+//    newPassWord.text = @"新密码";
+//    newPassWord.font = [UIFont fontWithName:@"GurmukhiMN" size:14];
+//    [self.view addSubview:newPassWord];
     
-    newPassWordTextField = [[UITextField alloc] initWithFrame:CGRectMake(92, 115, 200, 30)];
-    newPassWordTextField.placeholder = @"新密码6-24位";
+    newPassWordTextField = [[UITextField alloc] initWithFrame:CGRectMake(22,51,276,47)];
+    newPassWordTextField.placeholder = @"新密码6-20位";
     newPassWordTextField.font = [UIFont systemFontOfSize:14];
     newPassWordTextField.delegate = self;
     newPassWordTextField.secureTextEntry = YES;
     newPassWordTextField.clearButtonMode = UITextFieldViewModeAlways;
-    [newPassWordTextField setValue:[UIFont boldSystemFontOfSize:14] forKeyPath:@"_placeholderLabel.font"];
-    [self.view addSubview:newPassWordTextField];
+    //[newPassWordTextField setValue:[UIFont boldSystemFontOfSize:14] forKeyPath:@"_placeholderLabel.font"];
+    [topView addSubview:newPassWordTextField];
     
-    UIImageView *lineImage2 = [[UIImageView alloc] initWithFrame:CGRectMake(15, 150, 290, 1)];
+    UIImageView *lineImage2 = [[UIImageView alloc] initWithFrame:CGRectMake(20, 49, 280, 1)];
     [lineImage2 setBackgroundColor:[UIColor blackColor]];
-    [self.view addSubview:lineImage2];
+    [topView addSubview:lineImage2];
     lineImage2.alpha = 0.2;
     
-    UILabel *newAgainPassWord = [[UILabel alloc] initWithFrame:CGRectMake(15, 160, 60, 30)];
-    newAgainPassWord.textColor = BlueColor;
-    newAgainPassWord.text = @"重复密码";
-    newAgainPassWord.font = [UIFont fontWithName:@"GurmukhiMN" size:14];
-    [self.view addSubview:newAgainPassWord];
+//    UILabel *newAgainPassWord = [[UILabel alloc] initWithFrame:CGRectMake(15, 160, 60, 30)];
+//    newAgainPassWord.textColor = BlueColor;
+//    newAgainPassWord.text = @"重复密码";
+//    newAgainPassWord.font = [UIFont fontWithName:@"GurmukhiMN" size:14];
+//    [self.view addSubview:newAgainPassWord];
     
-    newAgainPassWordTextField = [[UITextField alloc] initWithFrame:CGRectMake(92, 160, 200, 30)];
+    newAgainPassWordTextField = [[UITextField alloc] initWithFrame:CGRectMake(22,100,276,47)];
     newAgainPassWordTextField.placeholder = @"再次输入";
     newAgainPassWordTextField.font = [UIFont systemFontOfSize:14];
     newAgainPassWordTextField.delegate = self;
     newAgainPassWordTextField.secureTextEntry = YES;
     newAgainPassWordTextField.clearButtonMode = UITextFieldViewModeAlways;
-    [newAgainPassWordTextField setValue:[UIFont boldSystemFontOfSize:14] forKeyPath:@"_placeholderLabel.font"];
-    [self.view addSubview:newAgainPassWordTextField];
+    //[newAgainPassWordTextField setValue:[UIFont boldSystemFontOfSize:14] forKeyPath:@"_placeholderLabel.font"];
+    [topView addSubview:newAgainPassWordTextField];
     
-    UIImageView *lineImage3 = [[UIImageView alloc] initWithFrame:CGRectMake(15, 195, 290, 1)];
-    [lineImage3 setBackgroundColor:[UIColor blackColor]];
-    [self.view addSubview:lineImage3];
-    lineImage3.alpha = 0.2;
+//    UIImageView *lineImage3 = [[UIImageView alloc] initWithFrame:CGRectMake(20, 158, 280, 1)];
+//    [lineImage3 setBackgroundColor:[UIColor blackColor]];
+//    [topView addSubview:lineImage3];
+//    lineImage3.alpha = 0.2;
     
     confirmBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     confirmBtn.frame = CGRectMake(22, 500, 276, 42);
     [confirmBtn setImage:[GetImagePath getImagePath:@"用户条款_05"] forState:UIControlStateNormal];
     [confirmBtn addTarget:self action:@selector(updataPassWordAction) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:confirmBtn];
+    
+    [EndEditingGesture addGestureToView:self.view];
 }
 
 - (void)didReceiveMemoryWarning
@@ -175,27 +184,19 @@
 
 -(void)updataPassWordAction{
     if(newPassWordTextField.text.length<6){
-        [RemindView remindViewWithContent:@"密码大于6位" superView:self.view centerY:230];
+        [RemindView remindViewWithContent:@"密码大于6位" superView:self.view centerY:260];
         return;
     }
     
     NSRange newPassWordTextFieldRange = [newPassWordTextField.text rangeOfString:@" "];
     if (newPassWordTextFieldRange.location != NSNotFound) {
         //有空格
-        [RemindView remindViewWithContent:@"密码不能包含空格" superView:self.view centerY:230];
+        [RemindView remindViewWithContent:@"密码不能包含空格" superView:self.view centerY:260];
         return;
     }
     
-//    if(![self LetterNoErr:newPassWordTextField.text]){
-//        return;
-//    }
-//    
-//    if(![self NumberNoErr:newPassWordTextField.text]){
-//        return;
-//    }
-    
     if(![newPassWordTextField.text isEqualToString:newAgainPassWordTextField.text]){
-        [RemindView remindViewWithContent:@"密码输入不一致，请重新输入" superView:self.view centerY:230];
+        [RemindView remindViewWithContent:@"密码输入不一致，请重新输入" superView:self.view centerY:260];
     }else{
         NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
         [dic setValue:[MD5 md5HexDigest:oldPassWordTextField.text] forKey:@"oldpassword"];
@@ -226,7 +227,7 @@
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"[A-Za-z]" options:NSRegularExpressionCaseInsensitive error:nil];
     NSUInteger numberOfMatches = [regex numberOfMatchesInString:phone options:0 range:NSMakeRange(0, [phone length])];
     if (numberOfMatches ==phone.length) {
-        [RemindView remindViewWithContent:@"密码不能为全英文" superView:self.view centerY:210];
+        [RemindView remindViewWithContent:@"密码不能为全英文" superView:self.view centerY:260];
         return NO;
     }
     return YES;
@@ -238,7 +239,7 @@
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"[0-9]" options:NSRegularExpressionCaseInsensitive error:nil];
     NSUInteger numberOfMatches = [regex numberOfMatchesInString:phone options:0 range:NSMakeRange(0, [phone length])];
     if (numberOfMatches ==phone.length) {
-        [RemindView remindViewWithContent:@"密码不能为全数字" superView:self.view centerY:210];
+        [RemindView remindViewWithContent:@"密码不能为全数字" superView:self.view centerY:260];
         return NO;
     }
     return YES;
@@ -249,7 +250,7 @@
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"[-@_]" options:NSRegularExpressionCaseInsensitive error:nil];
     NSUInteger numberOfMatches = [regex numberOfMatchesInString:phone options:0 range:NSMakeRange(0, [phone length])];
     if (numberOfMatches ==phone.length) {
-        [RemindView remindViewWithContent:@"密码不能为全符号" superView:self.view centerY:210];
+        [RemindView remindViewWithContent:@"密码不能为全符号" superView:self.view centerY:260];
         return NO;
     }
     return YES;
