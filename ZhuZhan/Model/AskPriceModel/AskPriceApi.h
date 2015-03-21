31087d96adc88ca1ad01b4da4725cdfbc053e93a
wobushigogo <wangy322@sina.com>
2015-03-21 13:18:30 +0800

    wy 询价详情接口

--- ZhuZhan/Model/AskPriceModel/AskPriceApi.h
+++ ZhuZhan/Model/AskPriceModel/AskPriceApi.h
@@ -14,4 +14,7 @@
 
 //获取列表
 + (NSURLSessionDataTask *)GetAskPriceWithBlock:(void (^)(NSMutableArray *posts, NSError *error))block status:(NSString *)status startIndex:(int)startIndex noNetWork:(void(^)())noNetWork;
+
+//获取询价列表详情
++ (NSURLSessionDataTask *)GetAskPriceDetailsWithBlock:(void (^)(NSMutableArray *posts, NSError *error))block tradeId:(NSString *)tradeId noNetWork:(void(^)())noNetWork;
 @end
