cf339b91510052ae588ac16bc69053113c91349c
孙元侃 <sunyuankan@sunyuankandeMac-mini.local>
2015-03-24 17:21:52 +0800

    需求列表更新

--- ZhuZhan/Business/AskPrice/AskPriceViewController.m
+++ ZhuZhan/Business/AskPrice/AskPriceViewController.m
@@ -130,7 +130,7 @@
         cell.clipsToBounds=YES;
     }
     AskPriceModel *model = self.showArr[indexPath.row];
-    cell.contents=@[model.a_invitedUser,model.a_productBigCategory,model.a_productCategory,model.a_remark,model.a_tradeStatus];
+    cell.contents=@[model.a_invitedUser,model.a_productBigCategory,model.a_productCategory,model.a_remark,model.a_category,model.a_tradeStatus];
     return cell;
 }
 
