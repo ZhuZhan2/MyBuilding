22744f17de2e24b256f362e903c7fae80c21bdea
孙元侃 <sunyuankan@sunyuankandeMac-mini.local>
2015-05-12 10:33:40 +0800

    孙元侃 公司详情修改

--- ZhuZhan/Company/CompanyDetailViewController.m
+++ ZhuZhan/Company/CompanyDetailViewController.m
@@ -351,6 +351,7 @@
         }
     }else{
         LoginViewController *loginVC = [[LoginViewController alloc] init];
+        loginVC.needDelayCancel = YES;
         loginVC.delegate = self;
         UINavigationController *nv = [[UINavigationController alloc] initWithRootViewController:loginVC];
         [self.view.window.rootViewController presentViewController:nv animated:YES completion:nil];
@@ -365,6 +366,7 @@
         [alertView show];
     }else{
         LoginViewController *loginVC = [[LoginViewController alloc] init];
+        loginVC.needDelayCancel = YES;
         loginVC.delegate = self;
         UINavigationController *nv = [[UINavigationController alloc] initWithRootViewController:loginVC];
         [self.view.window.rootViewController presentViewController:nv animated:YES completion:nil];
