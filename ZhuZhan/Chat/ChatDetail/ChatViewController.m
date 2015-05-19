d15cbaa6e6122904988cbefc6b492eea30f75a32
wobushigogo <wangy322@sina.com>
2015-05-19 09:54:38 +0800

    wy

--- ZhuZhan/Chat/ChatDetail/ChatViewController.m
+++ ZhuZhan/Chat/ChatDetail/ChatViewController.m
@@ -299,7 +299,9 @@
     [app.socket readDataWithTimeout:-1 tag:0];
 }
 
-- ch
+- (void)cameraWillFinishWithLowQualityImage:(UIImage *)lowQualityimage originQualityImage:(UIImage *)originQualityImage isCancel:(BOOL)isCancel{
+    NSLog(@"lowQualityimage=%@",lowQualityimage);
+}
 
 -(void)addModelWithContent:(NSString*)content{
     ChatMessageModel* model=[[ChatMessageModel alloc]init];
