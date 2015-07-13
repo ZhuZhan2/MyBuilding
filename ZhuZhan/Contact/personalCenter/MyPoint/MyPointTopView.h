9ecb8cdf00941cef82e1b9ac7b64c3d23f494e69
wobushigogo <wangy322@sina.com>
2015-07-13 18:10:57 +0800

    wy

--- ZhuZhan/Contact/personalCenter/MyPoint/MyPointTopView.h
+++ ZhuZhan/Contact/personalCenter/MyPoint/MyPointTopView.h
@@ -14,6 +14,17 @@
 
 @interface MyPointTopView : UIView
 @property(nonatomic,weak)id<MyPointTopViewDelegate>delegate;
+/**
+ *  从外部设置积分
+ *
+ *  @param point 传入的积分值
+ */
 -(void)setPoint:(NSString *)point;
+
+/**
+ *  从外部设置是否异常
+ *
+ *  @param status 异常的判断 00正常 01不正常
+ */
 -(void)setStatus:(NSString *)status;
 @end
