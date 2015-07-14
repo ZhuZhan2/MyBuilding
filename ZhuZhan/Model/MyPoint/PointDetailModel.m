829f0d8b6697b55454742a6af49b6d1c64755062
wobushigogo <wangy322@sina.com>
2015-07-14 11:33:28 +0800

    wy

--- ZhuZhan/Model/MyPoint/PointDetailModel.m
+++ ZhuZhan/Model/MyPoint/PointDetailModel.m
@@ -14,7 +14,7 @@
     self.a_points = [dict[@"points"] intValue];
     if([dict[@"hasSign"] isEqualToString:@"0"]){
         self.a_hasSign = NO;
-        self.a_todayGet = [NSString stringWithFormat:@"今天签到可获得%@积分",dict[@"toDayGet"]];
+        self.a_todayGet = [NSString stringWithFormat:@"今天签到可获得%@积分",dict[@"todayGet"]];
     }else{
         self.a_hasSign = YES;
         self.a_todayGet = [NSString stringWithFormat:@"明天签到可获得%@积分",dict[@"tomorrowGet"]];
