2a2eccf94d3df2625f2778b5299fa8769cce3dcc
孙元侃 <sunyuankan@sunyuankandeMac-mini.local>
2015-07-16 13:48:05 +0800

    孙元侃 webview链接修改

--- ZhuZhan/Contact/personalCenter/MyPoint/PointRule/PointRuleViewController.m
+++ ZhuZhan/Contact/personalCenter/MyPoint/PointRule/PointRuleViewController.m
@@ -20,19 +20,18 @@
     [self initNavi];
     [self initStageChooseViewWithStages:@[@"获得途径",@"注意事项"] numbers:nil underLineIsWhole:YES normalColor:AllLightGrayColor highlightColor:BlueColor];
     [self.view insertSubview:self.webView1 atIndex:0];
+    [self stageBtnClickedWithNumber:0];
 }
 
 - (void)stageBtnClickedWithNumber:(NSInteger)stageNumber{
-    
+    NSURL* url = [NSURL URLWithString:@[@"http://121.40.127.189:8090/wechat/integralAccess.html",@"http://121.40.127.189:8090/wechat/integralNote.html"][stageNumber]];
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    [_webView1 loadRequest:request];
 }
 
 - (UIWebView *)webView1{
     if (!_webView1) {
         _webView1 = [[UIWebView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.stageChooseView.frame), kScreenWidth, kScreenHeight-CGRectGetMaxY(self.stageChooseView.frame))];
-        
-        NSURL* url = [NSURL URLWithString:@"http://www.baidu.com"];
-        NSURLRequest* request = [NSURLRequest requestWithURL:url];
-        [_webView1 loadRequest:request];
     }
     return _webView1;
 }
