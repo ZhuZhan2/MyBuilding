0a8060fec6b41e82374daf94de8fced670233343
孙元侃 <sunyuankan@sunyuankandeMac-mini.local>
2015-07-16 13:52:31 +0800

    孙元侃 积分规则webview修改

--- ZhuZhan/Contact/personalCenter/MyPoint/PointRule/PointRuleViewController.m
+++ ZhuZhan/Contact/personalCenter/MyPoint/PointRule/PointRuleViewController.m
@@ -9,8 +9,7 @@
 #import "PointRuleViewController.h"
 
 @interface PointRuleViewController ()
-@property (nonatomic, strong)UIWebView* webView1;
-@property (nonatomic, strong)UIWebView* webView2;
+@property (nonatomic, strong)UIWebView* webView;
 @end
 
 @implementation PointRuleViewController
@@ -19,21 +18,21 @@
     [super viewDidLoad];
     [self initNavi];
     [self initStageChooseViewWithStages:@[@"获得途径",@"注意事项"] numbers:nil underLineIsWhole:YES normalColor:AllLightGrayColor highlightColor:BlueColor];
-    [self.view insertSubview:self.webView1 atIndex:0];
+    [self.view insertSubview:self.webView atIndex:0];
     [self stageBtnClickedWithNumber:0];
 }
 
 - (void)stageBtnClickedWithNumber:(NSInteger)stageNumber{
     NSURL* url = [NSURL URLWithString:@[@"http://121.40.127.189:8090/wechat/integralAccess.html",@"http://121.40.127.189:8090/wechat/integralNote.html"][stageNumber]];
     NSURLRequest* request = [NSURLRequest requestWithURL:url];
-    [_webView1 loadRequest:request];
+    [_webView loadRequest:request];
 }
 
-- (UIWebView *)webView1{
-    if (!_webView1) {
-        _webView1 = [[UIWebView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.stageChooseView.frame), kScreenWidth, kScreenHeight-CGRectGetMaxY(self.stageChooseView.frame))];
+- (UIWebView *)webView{
+    if (!_webView) {
+        _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.stageChooseView.frame), kScreenWidth, kScreenHeight-CGRectGetMaxY(self.stageChooseView.frame))];
     }
-    return _webView1;
+    return _webView;
 }
 
 /**
