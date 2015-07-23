eb6f506176019efbb77635dacc4b5a2cf1fe5488
wobushigogo <wangy322@sina.com>
2015-07-23 13:42:05 +0800

    wy

---
+++ ZhuZhan/UIApplication+GCovFlush.m
@@ -0,0 +1,26 @@
+//
+//  UIApplication+GCovFlush.m
+//  TestOutCoverage
+//
+//  Created by LeRoy Mattingly on 9/12/13.
+//  Copyright (c) 2013 OpenSource. All rights reserved.
+//
+
+#import "UIApplication+GCovFlush.h"
+#import "GcovExtern.h"
+
+@implementation UIApplication (GCovFlush)
+
++ (void)forceGcovFlush
+{
+    if (__gcov_flush) {
+        __gcov_flush();
+    }
+}
+
++ (void)load
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(forceGcovFlush) name:UIApplicationDidEnterBackgroundNotification object:nil];
+}
+
+@end
---
+++ ZhuZhanTests/XCTest+GCovFlush.m
@@ -0,0 +1,32 @@
+//
+//  XCTest+GCovFlush.m
+//  TestOutCoverage
+//
+//  Created by LeRoy Mattingly on 9/12/13.
+//  Copyright (c) 2013 OpenSource. All rights reserved.
+//
+
+#import "XCTest+GCovFlush.h"
+#import "GcovExtern.h"
+#import <objc/runtime.h>
+
+@implementation XCTest (GCovFlush)
+
++ (void)load
+{
+    Method original, swizzled;
+    
+    original = class_getInstanceMethod(self, @selector(tearDown));
+    swizzled = class_getInstanceMethod(self, @selector(_swizzledTearDown));
+    method_exchangeImplementations(original, swizzled);
+}
+
+- (void)_swizzledTearDown
+{
+    if (__gcov_flush) {
+        __gcov_flush();
+    }
+        [self _swizzledTearDown];
+}
+
+@end
